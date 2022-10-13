c928f5e8a6f1c1b24d35c00003b9aedb22859c96
Kirill_Moiseev_qwertyuiopoiuyhgtfdsdfgh <kilkuruzzza@yndex.ru>
2022-10-13 14:08:49 +0300

    1

---
+++ 57/Program.cs
@@ -0,0 +1,97 @@
+﻿//Задайте двумерный массив. Напишите программу, 
+//которая поменяет местами первую и последнюю строку массива.
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+WriteLine();
+int[] rowAr=GetRowArray(array);
+SortArray(rowAr);
+WriteLine(String.Join(" ",rowAr));
+PrintData(rowAr);
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+
+int[] GetRowArray(int[,] inArray)
+{
+    int[] result = new int[inArray.GetLength(0)*inArray.GetLength(1)];
+    int index=0;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            result[i * inArray.GetLength(1) + j] = inArray[i,j];
+            index++;
+        }
+    }
+    return result;
+}
+
+void SortArray(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        for (int j = i+1; j < inArray.Length; j++)
+        {
+            if(inArray[i]>inArray[j])
+            {
+                int k=inArray[i];
+                inArray[i]=inArray[j];
+                inArray[j]=k;
+            }
+        }
+    }
+}
+
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+void PrintData(int[] inArray)
+{
+    int el=inArray[0];
+    int count = 1;
+    for (int i = 1; i < inArray.Length; i++)
+    {
+        if(inArray[i]!=el)
+        {
+            WriteLine($"{el} встречается {count}");
+            el=inArray[i];
+            count=1;
+        }
+        else
+        {
+            count++;
+        }
+    }
+    WriteLine($"{el} встречается {count}");
+}
+
---
+++ 59/Program.cs
@@ -0,0 +1,86 @@
+﻿//Из двумерного массива целых чисел удалить строку и столбец, 
+//на пересечении которых расположен первый найденный наименьший элемент.
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+WriteLine();
+WriteLine($"Минимальный элемент находится -> {String.Join(" ",GetIndexMinEl(array))}" );
+WriteLine();
+PrintArray(GetResultArray(array, GetIndexMinEl(array)));
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+// Функция для поиска идекса минимального элемента
+int[] GetIndexMinEl(int[,] inArray)
+{
+    int[] result = new int[]{0,0};
+    int min=inArray[0,0];
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if(min>inArray[i,j])
+            {
+                min=inArray[i,j];
+                result[0]=i;
+                result[1]=j;
+            }
+        }
+    }
+    return result;
+}
+//Функция для финального массива,без строчки и стобца.В ней мы 
+//ничего не удаляем,просто создаем новый массив и записываем в него все кроме стобца и строки 
+//которые нужно удалить
+int[,] GetResultArray(int[,] inArray, int[] indexes) // indexes[0] = 2, indexes[1] = 2 
+{
+    int[,] result = new int[inArray.GetLength(0) - 1, inArray.GetLength(1) - 1];
+    int row = 0;
+    int column = 0;
+    for (int i = 0; i < inArray.GetLength(0); i++)// перебор строк 
+    {
+        if (i == indexes[0]) continue;
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (j == indexes[1]) continue;
+            result[row, column] = inArray[i, j];
+            column++;
+        }
+        column = 0;
+        row++;
+    }
+    return result;
+}
+
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
